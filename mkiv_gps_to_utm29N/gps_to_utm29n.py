5334565c79dc10ad048c2a5517de237fa91c2633
DIJALS RAI 06021124 <06021124@uhi.ac.uk>
2017-09-26 15:54:41 +0100

    finding the position of the radar and its coverage, converting the gps to UTM29 North values, and locating the position of the MKIV and MKIII

--- mkiv_gps_to_utm29N/gps_to_utm29n.py
+++ mkiv_gps_to_utm29N/gps_to_utm29n.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import pyproj
 from datetime import datetime
 
-def convertCoords(row):
+def convertCoords(dictionary):
     
     """ This Converts lat and long values into a UTM 29 North values"""
     
     inProj = pyproj.Proj(init='epsg:4326')
     outProj = pyproj.Proj(init='epsg:32629')
-    UTM29N_lon,UTM29N_lat = pyproj.transform(inProj,outProj,row['lon'],row['lat'])
+    UTM29N_lon,UTM29N_lat = pyproj.transform(inProj,outProj,dictionary['lon'],dictionary['lat'])
     UTM29N = {
         'UTM29N_lon' : UTM29N_lon,
         'UTM29N_lat' : UTM29N_lat
@@ -20,8 +20,13 @@ def convertCoords(row):
 
 def utm_of_the_polar_data(UTM29N, distance, columns, rows):
     
-    """ This takes the UTM29N values of the radar, the distance the radar covers, and the quantity of rows and columns for the whole rotation 
-        This returns a dictionary that holds the UTM29N values for the whole rotation     
+    """ 
+        This takes the UTM29N values of the radar, the distance the radar covers, 
+        and the quantity of rows and columns for the whole rotation. 
+        
+        This returns a dictionary that holds the UTM29N values for the whole rotation, 
+        and a Scatter Plot that shows the rotations and its UTM 29 North 
+        values. 
     """
 
     full_angle = np.pi * 2
@@ -45,18 +50,34 @@ def utm_of_the_polar_data(UTM29N, distance, columns, rows):
     
     xopp_and_yadj = { 'x_opp' : x_opp, 'y_adj' : y_adj}
     
+    plt.figure("fig3")
+    plt.scatter(xopp_and_yadj['x_opp'], xopp_and_yadj['y_adj'])
+    plt.show()
+    
     return xopp_and_yadj
 
 
-def mkiv_utm29n_values(angular_position_range, distance_position_range, xopp_and_yadj):
+def mk_utm29n_values(angular_position_range, distance_position_range, xopp_and_yadj):
     
-    """This takes a dictionary that contains the whole rotational utm29n values and extracts the utm29n values of the mkiv using the angular,
-    and distance position range of the mkiv"""
+    """
+    This takes a dictionary that contains the whole rotational utm29n values 
+    and extracts the utm29n values of the mkiv using the angular,
+    and distance position range of the mkiv
+    
+    This returns a dic with the mkiv or mkiii's UTM29N x and y positions in the 
+    radar and shows the scatter plot
+    """
     
     x_opp_df = pd.DataFrame(xopp_and_yadj['x_opp'])
     y_adj_df = pd.DataFrame(xopp_and_yadj['y_adj'])
-    mkiv_x_opp_df = x_opp_df.iloc[angular_position_range,distance_position_range]
-    mkiv_y_adj_df = y_adj_df.iloc[angular_position_range,distance_position_range]
+    mk_x_opp_df = x_opp_df.iloc[angular_position_range,distance_position_range]
+    mk_y_adj_df = y_adj_df.iloc[angular_position_range,distance_position_range]
     
-    mkiv_xopp_and_yadj_utm29n = {'mkiv_x_opp_df' : mkiv_x_opp_df, 'mkiv_y_adj_df': mkiv_y_adj_df}
-    return mkiv_xopp_and_yadj_utm29n
+    mk_xopp_and_yadj_utm29n = {'mk_x_opp_df' : mk_x_opp_df, 'mk_y_adj_df': mk_y_adj_df}
+    
+    plt.figure("fig3")
+    plt.scatter(mk_xopp_and_yadj_utm29n['mk_x_opp_df'], mk_xopp_and_yadj_utm29n['mk_y_adj_df'])
+    plt.show()
+    
+    return mk_xopp_and_yadj_utm29n
+
